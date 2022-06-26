8a3a8c4f851e3f7fd5cb89fbd4e81dc33e3f6f37
Boris Lepikhin <boris@lepikhin.com>
2022-06-26 01:19:32 -0700

    wip

--- resources/js/Plugins/notifications.ts
+++ resources/js/Plugins/notifications.ts
@@ -3,16 +3,19 @@ import { Inertia } from "@inertiajs/inertia"
 import { usePage } from "@inertiajs/inertia-vue3"
 import { useToast } from "vue-toastification"
 
-const { success } = useToast()
+const { success, error } = useToast()
 
 export const notifications: Plugin = {
   install(app: App) {
     Inertia.on("finish", (event) => {
-      const { props } = usePage<{ flash?: { success: string } }>()
-      const flash = props.value.flash?.success
+      const { props } = usePage<{ flash?: { success: string; error: string } }>()
 
-      if (flash) {
-        success(flash)
+      if (props.value.flash?.success) {
+        success(props.value.flash?.success)
+      }
+
+      if (props.value.flash?.error) {
+        error(props.value.flash?.error)
       }
     })
   },
--- resources/js/modal/index.ts
+++
@@ -1,3 +0,0 @@
-export { plugin as modal } from "./plugin"
-export { useModal } from "./use-modal"
-export { Modal } from "./modal"
